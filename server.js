bb23170c9cd2ceeddf067f6b210815dbf1873575
Swafey Karanja <kswafey@gmail.com>
2025-03-26 17:00:31 +0300

    test ssh

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var schema = buildSchema(`
   type Query {
     hello: String
     age: Int
+    death: Boolean
   }
 `)
  
-var rootValue = { hello: () => "Hello world!", age: () => 30}
+var rootValue = { hello: () => "Hello world!", age: () => 30, death: false }
  
-var source = "{ hello, age }"
+var source = "{ hello, age, death}"
  
 graphql({ schema, source, rootValue }).then(response => {
   console.log(response)
