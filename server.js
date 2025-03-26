c8f1096b253c96e1735c0499a11670b441c64681
Swafey Karanja <kswafey@gmail.com>
2025-03-26 14:26:45 +0300

    GraphQl with nodeJs

---
+++ server.js
@@ -0,0 +1,16 @@
+var { graphql, buildSchema } = require("graphql")
+ 
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    age: Int
+  }
+`)
+ 
+var rootValue = { hello: () => "Hello world!", age: () => 30}
+ 
+var source = "{ hello, age }"
+ 
+graphql({ schema, source, rootValue }).then(response => {
+  console.log(response)
+})
