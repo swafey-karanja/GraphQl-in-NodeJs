5504b7689b3e29e8ebf36234f9b8fe1f35c01201
Swafey Karanja <kswafey@gmail.com>
2025-03-27 14:05:48 +0300

    day-1

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require("graphql")
+var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { createHandler } = require("graphql-http");
  
 var schema = buildSchema(`
   type Query {
@@ -12,6 +14,10 @@ var rootValue = { hello: () => "Hello world!", age: () => 30, death: false }
  
 var source = "{ hello, age, death}"
  
-graphql({ schema, source, rootValue }).then(response => {
-  console.log(response)
-})
+const app = express();
+
+app.all('/graphql', createHandler({ schema, rootValue }));
+
+app.listen(4000);
+
+console.log("App is running on localhost port 4000")
